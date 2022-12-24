0f0da1c190dfc07d654f28b977f3f0601d9d9f6f
Ron Jean-Francois <rjfrancoisjr@icloud.com>
2022-12-23 20:08:48 -0500

    new: add string replace method example

---
+++ python/examples/string-replace-method.py
@@ -0,0 +1,8 @@
+message1 = "Hi, I'm a Python Expert!"
+new_message1 = message1.replace("Python", "Ruby")  
+print(new_message1)
+#  Hi, I'm a Ruby Expert!
+
+message2 = "Where can you find what you need to finish the project that you just started."
+new_message2 = message2.replace("you", "I", 2)  
+print(new_message2)
